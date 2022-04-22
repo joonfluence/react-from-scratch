115b491fc49f0a257b4d16e7af85228929288419
Joon-ho Lee <joonho.lee@corretto.io>
2022-04-22 14:33:24 +0900

    prettier/eslint 설정 추가

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Wrapper">
+      <div className="App">
+        <h1>Hello, World! </h1>
+        <h2>Hello, World! </h2>
+        <h3>Hello, World! </h3>
+        <h4>Hello, World! </h4>
+        <h5>Hello, World! </h5>
+        <h6>Hello, World! </h6>
+      </div>
+    </div>
+  );
+}
+
+export default hot(module)(App);
