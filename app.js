fae40cd58a2708417298420fb349786478944cae
joonho lee <goodfluence@naver.com>
2022-09-18 16:31:55 +0900

    refactoring : 함수화

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+function createH1(props) {
+  return [document.createElement("h1")].map((element) => {
+    Object.entries({ ...props, "data-id": "subject" }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  })[0];
+}
+
+function createDiv(props) {
+  return [document.createElement("div")].map((element) => {
+    Object.entries({ ...props, "data-id": "layout" }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  })[0];
+}
+
 function createElement(type, props) {
   switch (type) {
     case "h1":
-      return [document.createElement("h1")].map((element) => {
-        Object.entries({ ...props, "data-id": "subject" }).forEach(([name, value]) =>
-          element.setAttribute(name, value)
-        );
-        return element;
-      })[0];
+      createH1(props);
     case "div":
-      return [document.createElement("div")].map((element) => {
-        Object.entries({ ...props, "data-id": "layout" }).forEach(([name, value]) =>
-          element.setAttribute(name, value)
-        );
-      });
+      createDiv(props);
   }
 }
