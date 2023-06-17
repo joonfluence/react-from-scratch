22fa58cc3a297256d33b11a159e8107afb95541b
joonho lee <joonfluence.dev@gmail.com>
2023-06-17 21:03:09 +0900

    [CONFIG] React to Next.js

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+const Title = () => {
+  return (
+    <h1>React to Next.js 🚀</h1>
+  )
+}
+
+const Home = () => {
+  return (
+    <>
+      <Title />
+      <div>Home</div>
+    </>
+  )
+}
+
+export default Home;
